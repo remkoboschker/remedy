a211e71e20c11a0f05c87bcb9a1a480ffdd7e0a7
Remko Boschker <remko@informatietuin.nl>
2012-04-18 15:42:53 +0200

    add new files

---
+++ employee.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose')
+,   Schema = mongoose.Schema;
+
+employeeSchema = new Schema({
+    personal: {
+      initials: 'initialen'
+      givenName: 'voornaam'
+      familyName: 'achternaam'
+      dateOfBirth: 'geboortedatum'
+      sex: 'geslacht'
+      idNumber: 'bsn'
+      idDocument: 'legitimatie'
+    },
+    photo: 'profielfoto'  
+    tel:    
+      work: 'tel_werk'
+      private: 'tel_prive'
+    mail:
+      work: 'mail_werk'
+      private: 'mail_prive' 
+    ice:
+      tel: 'ice_tel'
+      name: 'ice_naam'
+      relation: 'ice_relatie'
+    address:
+      street: 'straat'
+      number: 'huisnummer'
+      extension: 'huisnummer_toevoeging'
+      postalcode: 'postcode'
+      city: 'woonplaats'
+      country: 'land'
+    employed:
+      role: 'rol'
+      bankAccountNumber: 'bankrekening'
+      inService: 'in_dienst'
+      outService: 'uit_dienst'
+      cv: 'cv'
+      contract: 'contract'
+    
+})
